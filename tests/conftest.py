ba1c35b762bda29f1483774f0f827c5ae5614446
Parth Verma <parthvermaofficial007@gmial.com>
2026-08-12 01:22:47 +0530

    async fixture modified

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,18 @@
 # pyrefly: ignore [missing-import]
-import pytest
+import pytest_asyncio
 # pyrefly: ignore [missing-import]
 from httpx import AsyncClient, ASGITransport
+# pyrefly: ignore [missing-import]
+from asgi_lifespan import LifespanManager
 from app.main import app
 
-# Tells pytest to run async test
-@pytest.fixtures(scope="session")
-def anyio_backend():
-    return "asyncio"
 
 # Creates an async HTTP client to test the endpoints
-@pytest.fixtures(scope="session")
+@pytest_asyncio.fixture(scope="session")
 async def async_client():
 
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport,base_url="http://test") as client:
-        yield client
+    # Using this to start the connections for testing as well(without starting the actual server)
+    async with LifespanManager(app):
+        transport = ASGITransport(app=app)
+        async with AsyncClient(transport=transport,base_url="http://test") as client:
+            yield client
